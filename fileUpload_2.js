24789fef012d537aefd1030e6fade7724ef52137
stanlypalathingal <[200~b9058657@newcastle.ac.uk~>
2024-10-17 14:21:43 +0100

    fileupload is working well

--- fileUpload_2.js
+++ fileUpload_2.js
@@ -7,12 +7,30 @@ http.createServer(function(req,res) {
     if(req.url == '/fileupload'){
     var form = new formidable.IncomingForm();
     form.parse(req,function(err,fields,files) {
-        var oldpath = files.filetoupload.filepath;
-        var newpath = ("/Users/stanlywilsonsac/Library/CloudStorage/OneDrive-Personal/Programing/node_leaning/"+files.filetoupload.name);   
+        if (err){
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.write('Error in parsing file');
+            return res.end();
+        }
+        files = files.fileupload[0];
+
+        var oldpath = files.filepath;
+        console.log(oldpath)
+
+        var newpath = ("/Users/stanlywilsonsac/Library/CloudStorage/OneDrive-Personal/Programing/node_leaning/"
+                        +files.originalFilename);   
+
+        if (!oldpath){
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.write('Error in uploading file');
+            return res.end();
+        }   
+
         fs.rename(oldpath,newpath,function(err){
             if(err) throw err;
-        res.write('File uploaded');
-        res.end();
+                res.write('File uploaded');
+                console.log(newpath);
+                res.end();
         });
     });
 } else{
@@ -20,7 +38,7 @@ http.createServer(function(req,res) {
     res.write('<form action = "fileupload" method= "post" enctype= "multipart/form-data">');
     res.write('<input type = "file" name = "fileupload"><br>');
     res.write('<input type = "submit">');
-    res.write('<form>');
+    res.write('</form>');
     return res.end();
     }
 }).listen(8080);
