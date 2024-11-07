1a39e27157877abc6e6888d168cf9f9faf1cb943
stanlypalathingal <b9058657@newcastle.ac.uk>
2024-11-07 13:45:59 +0000

    leaning HTML_1

--- demo_db_conncetion.js
+++ demo_db_conncetion.js
@@ -1,8 +1,21 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://127.0.0.1:27017/pharmaceutical";
+console.log("Connecting to database");
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function(err, client) {
+  if (err) {
+    console.log("Error in connecting to database");
+    throw err;
+  };
+
+  console.log("Connected to database");
+
   if (err) throw err;
   console.log("Database created!");
-  db.close();
+
+  // Access the database
+  var db = client.db("mydb");
+
+  // Close the connection
+  client.close();
 });
